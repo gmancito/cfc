75d76cdf0d26f6b5b9b647e4c796bdb763555b3d
Nick Stees <nstees@gmail.com>
2018-11-29 18:34:30 -0500

    Updated batch script for new project

--- core-remove.bat
+++ core-remove.bat
@@ -1,11 +1,11 @@
 @ECHO ON
-rmdir /Q /S "web/includes"
-rmdir /Q /S "web/misc"
-rmdir /Q /S "web/modules"
-rmdir /Q /S "web/profiles"
-rmdir /Q /S "web/scripts"
-rmdir /Q /S "web/themes"
-cd web
+rmdir /Q /S "public_html/includes"
+rmdir /Q /S "public_html/misc"
+rmdir /Q /S "public_html/modules"
+rmdir /Q /S "public_html/profiles"
+rmdir /Q /S "public_html/scripts"
+rmdir /Q /S "public_html/themes"
+cd public_html
 del ".editorconfig"
 del "authorize.php"
 del "CHANGELOG.txt"
@@ -24,6 +24,5 @@ del "README.txt"
 del "update.php"
 del "UPGRADE.txt"
 del "xmlrpc.php"
-ECHO Remember to restore the includes/database/sqlsrv drivers after copying new core
-ECHO Remember to apply patches such as DBLOG patches
+ECHO Remember to apply patches if ther are any!
 PAUSE
